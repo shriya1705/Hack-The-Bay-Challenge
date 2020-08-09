3f089e184a6c02f32505858600db7501953d9850
sydneyriemer <sydney.riemer@gmail.com>
2020-08-09 14:05:11 +0300

    commited

---
+++ dice_roller.py
@@ -0,0 +1,10 @@
+'''Dice roller game-Python Intermediate Course, GitHub'''
+
+def main():
+  import random
+  roll = random.randint(1,6)
+  print(f'You rolled a {roll}')
+ 
+
+if __name__== "__main__":
+  main()
